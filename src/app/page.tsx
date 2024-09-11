be6a9782aa3d96e79312fbf1f6b98ea397b3aa31
manav1411 <manavbdodia@gmail.com>
2024-09-11 12:38:37 +1000

    unified style across pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
                 {copied ? (
                   <IoMdCheckmark className="text-green-500 w-5 h-5 transition-colors duration-150 ease-in-out" />
                 ) : (
-                  <FaRegCopy className="text-gray-500 dark:text-gray-400 w-5 h-5 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-150 ease-in-out" />
+                  <FaRegCopy className="text-neutral-500 dark:text-neutral-400 w-5 h-5 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-150 ease-in-out" />
                 )}
               </button>
               {copied && <span className="mr-8 text-xs opacity-50">Copied!</span>}
@@ -77,7 +77,6 @@ export default function Home() {
       </div>
 
 
-
       {/* Projects */}
       <section className="w-full max-w-4xl">
         <div className="flex justify-between items-center mb-8">
@@ -86,7 +85,7 @@ export default function Home() {
           </h2>
           <Link
               href="/projects"
-              className={`px-4 py-2 border rounded-md transition text-gray-900 dark:text-gray-100 border-gray-400 dark:border-gray-500 hover:bg-gray-300 dark:hover:bg-gray-700`}
+              className={`px-4 py-2 border rounded-md transition text-neutral-900 dark:text-neutral-100 border-neutral-400 dark:border-neutral-500 hover:bg-neutral-300 dark:hover:bg-neutral-700`}
             >
               All Projects
           </Link>
@@ -99,8 +98,9 @@ export default function Home() {
       </section>
 
 
-          {/* divider */}
-          <div className="w-full max-w-4xl border-b border-gray-400 dark:border-gray-700 mb-16 mt-16"></div>
+      {/* divider */}
+      <div className="w-full max-w-4xl border-b border-neutral-400 dark:border-neutral-700 mb-16 mt-16"></div>
+
 
       {/* Resume */}
       <section className="w-full max-w-4xl mb-16">
@@ -108,7 +108,7 @@ export default function Home() {
           <h2 className="text-3xl font-semibold mb-6">Experience</h2>
           <Link
             href="/resume"
-            className="px-4 py-2 border rounded-md transition text-gray-900 dark:text-gray-100 border-gray-400 dark:border-gray-500 hover:bg-gray-300 dark:hover:bg-gray-700"
+            className="px-4 py-2 border rounded-md transition text-neutral-900 dark:text-neutral-100 border-neutral-400 dark:border-neutral-500 hover:bg-neutral-300 dark:hover:bg-neutral-700"
           >
             View resume
           </Link>
@@ -118,9 +118,9 @@ export default function Home() {
           <div key={index} className="mb-8">
             <div className="flex justify-between items-center mb-2">
               <h3 className="text-2xl font-semibold">{experience.title}</h3>
-              <p className="text-gray-600 dark:text-gray-400">{experience.year}</p>
+              <p className="text-neutral-600 dark:text-neutral-400">{experience.year}</p>
             </div>
-            <ul className="list-disc list-inside text-gray-500 dark:text-gray-400">
+            <ul className="list-disc list-inside text-neutral-500 dark:text-neutral-400">
               {experience.details.map((detail, i) => (
                 <li key={i} className="mb-2">{detail}</li>
               ))}
@@ -134,7 +134,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="mt-32 mb-5">
-        <p className="text-sm text-gray-500 text-center">
+        <p className="text-sm text-neutral-500 text-center">
         Made with ✨, powered by ☕
         <br />
         © 2024 Manav Dodia
